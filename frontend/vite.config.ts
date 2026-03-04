d856594719122ccd0b5f39ef26908edf44d2ab3b
angelct5 <angelct768@gmail.com>
2026-03-05 00:30:21 +0530

    updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ai-image-detector",
+  base: "/ai-generated-image-detector",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
