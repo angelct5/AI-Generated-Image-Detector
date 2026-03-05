96391bf7176a6355cef619e0e25826ff6bea8759
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2026-03-05 08:24:02 +0530

    base name changed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ai-generated-image-detector",
+  base: "/AI-Generated-Image-Detector",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
